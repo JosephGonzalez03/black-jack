ceca9320df40ea18234f4a433b5c1a23653036ff
JosephGonzalez03 <josephdg3@gmail.com>
2022-09-03 13:16:56 -0500

    refactor: Implement game using enums and structs

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,82 @@
 use std::io;
 use rand::Rng;
 
-fn main() {
-    let mut card_deck: [i32; 52] = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,-1,-1,-1,-1];
-    let mut player_hand: [i32; 52] = [0;52];
-    //let mut dealer_hand: [i32; 52] = [0;52];
-    let mut turn = 0;
+enum GameState {
+    WIN,
+    LOSE,
+    CONTINUE,
+}
 
-    let mut rng = rand::thread_rng();
+struct Game {
+    card_deck: [i32; 52],
+    player_hand: [i32; 52],
+    turn: usize,
+}
+
+impl Game {
+    fn draw_card(&mut self) -> i32 {
+        let mut rng = rand::thread_rng();
+        let mut new_card;
+        'draw: loop {
+            let new_index = rng.gen_range(0..=51);
+            new_card = self.card_deck[new_index];
+            if new_card != 0 {
+                if new_card == -1 {
+                    'ace_value: loop {
+                        let mut answer = String::new();
+                        println!("You drew an ace. Would you to treat it as a 1 or 11?");
+                        io::stdin()
+                            .read_line(&mut answer)
+                            .expect("Failed to read line.");
+
+                        let answer: i32 = answer.trim().parse().expect("Please type a number!");
+
+                        if answer == 1 || answer == 11 {
+                            new_card = answer;
+                            break 'ace_value;
+                        }
+                    }
+                }
+                self.card_deck[new_index] = 0;
+                break 'draw;
+            }
+        }
+        return new_card
+    }
+
+    fn add_to_hand(&mut self, card: i32) {
+        self.player_hand[self.turn] = card;
+    }
+
+    fn get_game_state(&self) -> GameState {
+        let mut sum = 0;
+        for card in self.player_hand {
+            sum = sum + card;
+        }
+        println!("You're hand is at {}", sum);
+
+        if sum > 21 {
+            return GameState::LOSE;
+        } else if sum == 21 {
+            return GameState::WIN;
+        }
+
+        return GameState::CONTINUE;
+
+    }
+
+    fn increase_turn(&mut self) {
+        self.turn = self.turn + 1;
+    }
+}
+
+fn main() {
+    //let mut dealer_hand: [i32; 52] = [0;52];
+    let mut game = Game {
+        card_deck: [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,-1,-1,-1,-1],
+        player_hand: [0;52],
+        turn: 0,
+    };
 
     'game: loop {
         let mut answer = String::new();
@@ -18,51 +87,21 @@ fn main() {
             .expect("Failed to read line.");
 
         if answer.trim().eq("y") {
-            let mut new_card;
-
-            'turn: loop {
-                let new_index = rng.gen_range(0..=51);
-                new_card = card_deck[new_index];
-                if new_card != 0 {
-                    card_deck[new_index] = 0;
-                    if new_card == -1 {
-                        'ace_value: loop {
-                            let mut answer = String::new();
-                            println!("You drew an ace. Would you to treat it as a 1 or 11?");
-                            io::stdin()
-                                .read_line(&mut answer)
-                                .expect("Failed to read line.");
-
-                            let answer: i32 = answer.trim().parse().expect("Please type a number!");
-
-                            if answer == 1 || answer == 11 {
-                                new_card = answer;
-                                break 'ace_value;
-                            }
-                        }
-                    }
-                    player_hand[turn] = new_card;
-                    println!("{}", new_card);
-
-                    let mut sum = 0;
-                    for card in player_hand {
-                        sum = sum + card;
-                    }
-                    println!("You're hand is at {}", sum);
-
-                    if sum > 21 {
-                        println!("You lose!");
-                        break 'game;
-                    } else if sum == 21 {
-                        println!("You win!");
-                        break 'game;
-                    }
-                    break 'turn;
+            let card = game.draw_card();
+            game.add_to_hand(card);
+            match game.get_game_state() {
+                GameState::CONTINUE => game.increase_turn(),
+                GameState::WIN => {
+                    println!("You win!");
+                    break 'game;
+                }
+                GameState::LOSE => {
+                    println!("You lose!");
+                    break 'game;
                 }
             }
         } else {
             println!("You are holding.");
         }
-        turn = turn + 1;
     }
 }
