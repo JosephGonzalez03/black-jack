a690571d8d0fcebbc4239913d9937c3c976c7508
JosephGonzalez03 <josephdg3@gmail.com>
2022-11-24 10:06:42 -0600

    refactor: Combine BlackJack instantiation and game call

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,5 @@ use crate::games::black_jack::BlackJack;
 pub mod games;
 
 fn main() {
-    //let mut dealer_hand: [i32; 52] = [0;52];
-    let game = BlackJack::new(2);
-
-    game.play();
+    BlackJack::new(4).play();
 }
